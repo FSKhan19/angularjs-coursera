a3d3f481759c4479ba318eea9138bcbc81facbb4
Farhan Shahid <farhanshahidkhan93@gmail.com>
2021-09-27 00:24:11 +0500

    Final

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,12 +1,37 @@
 (function() {
     'use strict';
-    angular.module('module1_sol_app', [])
-        .controller('MsgController', MsgController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-    MsgController.$inject = ['$scope'];
+    LunchCheckController.$inject = ['$scope'];
 
-    function MsgController($scope) {
-        $scope.name = "Farhan";
+    function LunchCheckController($scope) {
+
+
+        $scope.GetStatus = function(items) {
+            if (typeof items === "undefined" || items === "") {
+                $scope.message = "Please enter data first";
+                return '';
+            }
+
+            //removing white spaces
+            var without_white_spaces = items.trim().split(' ').join('');
+            // Get Item List
+            var items_array = without_white_spaces.split(',');
+            // removing ,, empty item list
+            var valid_items = items_array.filter(n => n);
+            var len = valid_items.length;
+            console.log(valid_items)
+
+            if (len <= 3) {
+                $scope.message = "Enjoy!";
+            } else if (len > 3) {
+                $scope.message = "Too much!"
+            } else {
+                $scope.message = "Please enter data first";
+            }
+
+        };
     }
 
 })();
